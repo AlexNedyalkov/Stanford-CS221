736667709e79ff13e98743875e201602e9dc4118
Alexander Nedyalkov <48474025+AlexNedyalkov@users.noreply.github.com>
2020-08-13 17:28:38 +0300

    Add files via upload
    FIxed Cache

--- compute_edit_distance.py
+++ compute_edit_distance.py
@@ -1,4 +1,5 @@
-def compute_edit_distance(s : str, t : str):
+def compute_edit_distance(s, t):
+    cache = {}  # (m, n) => result
     """
     Returns the maximum edit distance between s and t
     :param s: string s
@@ -7,19 +8,27 @@ def compute_edit_distance(s : str, t : str):
     """
     def recurse(m : int, n : int):
 
-        if m == 0:
-            return n
-        elif n == 0:
-            return m
+        if (m, n) in cache:
+            return cache[(m, n)]
+        if m == 0:  # Base case
+            result =  n
+        elif n == 0: # Base case
+            result =  m
         elif s[m - 1] == t[n - 1]: # Last letter matches
-            recurse(m-1, n-1)
+            return recurse(m-1, n-1)
         else:
 
             subCost = 1 + recurse(m -1, n - 1)
             delCost = 1 + recurse(m-1, n)
             insCost = 1 + recurse(m, n-1)
 
-            return min(subCost, delCost, insCost)
+            result =  min(subCost, delCost, insCost)
+
+        cache[(m, n)] = result
+
+        return result
 
 
     return recurse(len(s), len(t))
+
+print(compute_edit_distance("dcats" * 10, "cat" * 10))
